5337049e5f2b657ed372fb5b1fb5f695f95bab2b
jagatiadarsh23-eng <jagati.adarsh23@gmail.com>
2026-02-13 23:28:00 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,75 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Calculator")
+root.configure(bg="black")
+root.resizable(True, True)
+
+entry = tk.Entry(
+    root,
+    font=("Segoe UI", 20),
+    bg="dark gray",
+    fg="white",
+    bd=0,
+    justify="right"
+)
+entry.grid(row=0, column=0, columnspan=4, padx=12, pady=12, ipady=10)
+
+def press(v):
+    entry.insert(tk.END, v)
+
+def clear():ff
+    entry.delete(0, tk.END)
+
+def calc():
+    try:
+        result = eval(entry.get())
+        entry.delete(0, tk.END)
+        entry.insert(0, result)
+    except:
+        entry.delete(0, tk.END)
+        entry.insert(0, "Error")
+
+buttons = [
+    "7", "8", "9", "/",
+    "4", "5", "6", "*",
+    "1", "2", "3", "-",
+    "0", ".", "=", "+"
+]
+
+r = 1
+c = 0
+
+for b in buttons:
+    cmd = calc if b == "=" else lambda x=b: press(x)
+
+    tk.Button(
+        root,
+        text=b,
+        command=cmd,
+        font=("Segoe UI", 14),
+        width=5,
+        height=2,
+        bg="orange" if b in "+-*/=" else "dark gray",
+        fg="white",
+        bd=0
+    ).grid(row=r, column=c, padx=6, pady=6)
+
+    c += 1
+    if c > 3:
+        c = 0
+        r += 1
+
+tk.Button(
+    root,
+    text="C",
+    command=clear,
+    font=("Segoe UI", 14),
+    bg="red",
+    fg="white",
+    bd=0,
+    width=22,
+    height=2
+).grid(row=r, column=0, columnspan=4, pady=8)
+
+root.mainloop()
